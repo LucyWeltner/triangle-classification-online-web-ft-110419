af8b84b15078a7868b6e288ff2b1c40f0f95a166
Lucy Weltner <lucyiweltner@gmail.com>
2019-11-18 18:44:13 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,16 +1,16 @@
 class Triangle
   attr_accessor :length1, :length2, :length3
   
-  def initialization(lengtha, lengthb, lengthc)
+  def initialize(lengtha, lengthb, lengthc)
     @length1 = lengtha 
     @length2 = lengthb 
     @length3 = lengthc
   end 
   
   def kind
-    positive_lengths == @length1 < 0 && @length2 < 0 && @length3 < 0 
-    triangle_inequality == @length1 + @length2 > @length3 && @length3 + @length2 > @length1 && @length1 + @length3 > @length2
-    if positive_lengths && triangle_inequality 
+    positive_lengths = @length1 > 0 && @length2 > 0 && @length3 > 0 
+    triangle_inequality = @length1 + @length2 > @length3 && @length3 + @length2 > @length1 && @length1 + @length3 > @length2
+    if positive_lengths && triangle_inequality
       if @length1 == @length2 && @length2 == @length3 
         return :equilateral 
       elsif @length1 != @length2 && @length1 != @length3 && @length2 != @length3
@@ -19,17 +19,18 @@ class Triangle
         return :isosceles
       end 
     else 
-      begin 
-        raise TriangleError
-      rescue TriangleError => an_error
-        an_error.message 
-      end
+      # puts "Not a valid triangle!"
+      # begin 
+      raise TriangleError
+      # rescue TriangleError => an_error
+      #   puts an_error.message 
+      # end
     end 
   end 
   
   class TriangleError < StandardError 
     def message 
-      puts "Make sure your side lengths are all greater than 0, and the sum of the lengths of any two sides is greater than the length of the third side."
+      "Make sure your side lengths are all greater than 0, and the sum of the lengths of any two sides is greater than the length of the third side."
     end 
   end
 end
